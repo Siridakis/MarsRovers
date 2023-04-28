14c88efa5eca10a8cd87fd8890470e2e6231b31a
siridakis <eduardosiridakis@gmail.com>
2023-04-28 19:01:28 -0300

    [feature] database, [fix] small code cleanup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 import { InputForm } from './components/InputForm'
-import './styles/global.css'
+// import './styles/global.css'
 
 function App() {
 
   return (
     <div className='Mars h-screen'>
-      <h1 className='text-center text-lg font-bold mb-4 pt-4'> MARS ROVERS </h1>
+      <h1 className='p-4 bg-white bg-opacity-50 text-center text-lg font-bold mb-4 pt-4'> MARS ROVERS </h1>
       <div className='mx-8'>
         <InputForm/>
       </div>
--- web/src/components/InputForm.tsx
+++ web/src/components/InputForm.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from "react"
 
 export function InputForm() {
+  
   const [roverInput, setRoverInput] = useState({
     plateau: {
       x: 2,
@@ -20,9 +21,10 @@ export function InputForm() {
     }
   })
 
-  async function placeholderFun(event: FormEvent) {
+  const [output, setOutput] = useState('')
+
+  async function fetchSimulationData(event: FormEvent) {
     event.preventDefault()
-    // console.log(roverInput)
     const response = await fetch("http://localhost:3001/api/logs", {
         method: 'POST',
         headers: {
@@ -32,25 +34,27 @@ export function InputForm() {
         })
     const jsonData = await response.json()
     console.log(jsonData)
+    setOutput(jsonData)
     return
   }
 
+  const roverNumbers:[1,2] = [1,2]
+
   return (
-    <form onSubmit={event => placeholderFun(event)} className=" w-auto flex flex-col mt-6">
+    <>
+    <form onSubmit={event => fetchSimulationData(event)} className=" w-auto flex flex-col mt-6">
       <div className="w-auto flex mx-4 justify-between">
-        <div>
+        <div className="p-2 rounded-lg bg-white bg-opacity-50">
           <h1 className="font-semibold text-lg mb-4">Plateau Size</h1>
           <div className="flex content-center h-6 mb-2">
             <label htmlFor="plateauH" className="font-medium text-black leading-tight block align-middle w-36">
               Horizontal length:
             </label>
-            <input
+            <input className="ms-2 ps-4 rounded-lg w-24 text-center border border-black"
               type="number"
               min="2"
               id="plateauH"
-              placeholder=""
-              className="ms-2 ps-4 rounded-lg w-24 text-center border border-black"
-              autoFocus
+              defaultValue={2}
               onChange={event => setRoverInput(prevState => ({...prevState, plateau: {...prevState.plateau, x: parseInt(event.target.value)}}))}
             />
           </div>
@@ -58,131 +62,67 @@ export function InputForm() {
             <label htmlFor="plateauV" className="font-medium text-black leading-tight block align-middle w-36">
               Vertical length:
             </label>
-            <input
+            <input className="ms-2 ps-4 rounded-lg w-24 text-center border border-black"
               type="number"
               min="2"
               id="plateauV"
-              placeholder=""
-              className="ms-2 ps-4 rounded-lg w-24 text-center border border-black"
-              autoFocus
+              defaultValue={2}
               onChange={event => setRoverInput(prevState => ({...prevState, plateau: {...prevState.plateau, y: parseInt(event.target.value)}}))}
             />
           </div>
         </div>
-        <div>
-          <h1 className="font-semibold text-lg mb-4">Rover 1</h1>
+        {roverNumbers.map(num => {
+          return (
+        <div className="p-2 rounded-lg bg-white bg-opacity-50">
+          <h1 className="font-semibold text-lg mb-4">Rover {num}</h1>
           <h2 className="font-medium mb-2 align-middle">Landing position</h2>
           <div className="flex content-center h-6 my-2">
-            <label htmlFor="rover1X" className="font-medium text-black leading-tight block align-middle w-4">
+            <label htmlFor={`rover${num}X`} className="font-medium text-black leading-tight block align-middle w-4">
               X: 
             </label>
-            <input
+            <input className="mx-2 ps-4 rounded-lg w-16 text-center border border-black"
               type="number"
               min="0"
-              id="rover1X"
-              placeholder=""
-              className="mx-2 ps-4 rounded-lg w-16 text-center border border-black"
-              autoFocus
-              onChange={event => setRoverInput(prevState => ({...prevState, rover1: {...prevState.rover1, x: parseInt(event.target.value)}}))}
+              defaultValue={roverInput[`rover${num}`].x}
+              id={`rover${num}X`}
+              onChange={event => setRoverInput(prevState => ({...prevState, [`rover${num}`]: {...prevState[`rover${num}`], x: parseInt(event.target.value)}}))}
             />
-            <label htmlFor="rover1Y" className="font-medium text-black leading-tight block align-middle w-4">
+            <label htmlFor={`rover${num}Y`} className="font-medium text-black leading-tight block align-middle w-4">
               Y: 
             </label>
-            <input
+            <input className="mx-2 ps-4 rounded-lg w-16 text-center border border-black"
               type="number"
               min="0"
-              id="rover1Y"
-              placeholder=""
-              className="mx-2 ps-4 rounded-lg w-16 text-center border border-black"
-              autoFocus
-              onChange={event => setRoverInput(prevState => ({...prevState, rover1: {...prevState.rover1, y: parseInt(event.target.value)}}))}
+              defaultValue={roverInput[`rover${num}`].y}
+              id={`rover${num}Y`}
+              onChange={event => setRoverInput(prevState => ({...prevState, [`rover${num}`]: {...prevState[`rover${num}`], y: parseInt(event.target.value)}}))}
             />
-            <label htmlFor="rover1D" className="font-medium text-black leading-tight block align-middle w-32">
+            <label htmlFor={`rover${num}D`} className="font-medium text-black leading-tight block align-middle w-32">
               Facing direction: 
             </label>
-            <input
+            <input className="mx-2 rounded-lg w-16 text-center border border-black"
               type="text"
-              pattern="^[NnEeSsWw]$"
+              pattern="^[NESW]$"
               maxLength={1}
-              id="rover1D"
-              placeholder=""
-              className="mx-2 rounded-lg w-16 text-center border border-black"
-              autoFocus
-              onChange={event => setRoverInput(prevState => ({...prevState, rover1: {...prevState.rover1, facing: event.target.value}}))}
+              defaultValue={roverInput[`rover${num}`].facing}
+              id={`rover${num}D`}
+              onChange={event => setRoverInput(prevState => ({...prevState, [`rover${num}`]: {...prevState[`rover${num}`], facing: event.target.value}}))}
             />
           </div>
           <div className="flex content-center h-6 my-2">
-            <label htmlFor="rover1seq" className="font-medium text-black leading-tight block align-middle w-40">
+            <label htmlFor={`rover${num}seq`} className="font-medium text-black leading-tight block align-middle w-40">
               Movement sequence: 
             </label>
-            <input
+            <input className="mx-2 rounded-lg w-56 text-center border border-black"
               type="text"
-              pattern="[LlRrMm]*"
-              id="rover1seq"
-              placeholder=""
-              className="mx-2 rounded-lg w-56 text-center border border-black"
-              autoFocus
-              onChange={event => setRoverInput(prevState => ({...prevState, rover1: {...prevState.rover1, mov_sequence: event.target.value}}))}
+              pattern="[LRM]*"
+              id={`rover${num}seq`}
+              onChange={event => setRoverInput(prevState => ({...prevState, [`rover${num}`]: {...prevState[`rover${num}`], mov_sequence: event.target.value}}))}
             />
           </div>
         </div>
-        <div>
-          <h1 className="font-semibold text-lg mb-4">Rover 2</h1>
-          <h2 className="font-medium mb-2 align-middle">Landing position</h2>
-          <div className="flex content-center h-6 my-2">
-            <label htmlFor="rover2X" className="font-medium text-black leading-tight block align-middle w-4">
-              X: 
-            </label>
-            <input
-              type="number"
-              min="0"
-              id="rover2X"
-              placeholder=""
-              className="mx-2 ps-4 rounded-lg w-16 text-center border border-black"
-              autoFocus
-              onChange={event => setRoverInput(prevState => ({...prevState, rover2: {...prevState.rover2, x: parseInt(event.target.value)}}))}
-            />
-            <label htmlFor="rover2Y" className="font-medium text-black leading-tight block align-middle w-4">
-              Y: 
-            </label>
-            <input
-              type="number"
-              min="0"
-              id="rover2Y"
-              placeholder=""
-              className="mx-2 ps-4 rounded-lg w-16 text-center border border-black"
-              autoFocus
-              onChange={event => setRoverInput(prevState => ({...prevState, rover2: {...prevState.rover2, y: parseInt(event.target.value)}}))}
-            />
-            <label htmlFor="rover2D" className="font-medium text-black leading-tight block align-middle w-32">
-              Facing direction: 
-            </label>
-            <input
-              type="text"
-              pattern="^[NnEeSsWw]$"
-              maxLength={1}
-              id="rover2D"
-              placeholder=""
-              className="mx-2 rounded-lg w-16 text-center border border-black"
-              autoFocus
-              onChange={event => setRoverInput(prevState => ({...prevState, rover2: {...prevState.rover2, facing: event.target.value}}))}
-            />
-          </div>
-          <div className="flex content-center h-6 my-2">
-            <label htmlFor="rover2seq" className="font-medium text-black leading-tight block align-middle w-40">
-              Movement sequence: 
-            </label>
-            <input
-              type="text"
-              pattern="[LlRrMm]*"
-              id="rover2seq"
-              placeholder=""
-              className="mx-2 rounded-lg w-56 text-center border border-black"
-              autoFocus
-              onChange={event => setRoverInput(prevState => ({...prevState, rover2: {...prevState.rover2, mov_sequence: event.target.value}}))}
-            />
-          </div>
-        </div>
+        )
+        })}
       </div>
       <input
         type="submit"
@@ -191,5 +131,9 @@ export function InputForm() {
         autoFocus
         />
     </form>
+    <div className=" w-auto mx-4 p-2 rounded-lg bg-white bg-opacity-50">
+      <h1 className="font-semibold text-lg">Output: {output}</h1>
+    </div>
+    </>
   )
 }
